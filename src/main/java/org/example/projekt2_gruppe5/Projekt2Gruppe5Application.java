f23c42ce15b1876cfab62ac42a1e7e15db0b2c0f
safirestorm <sarahfagerstroem@gmail.com>
2025-03-31 16:25:58 +0200

    start

---
+++ src/main/java/org/example/projekt2_gruppe5/Projekt2Gruppe5Application.java
@@ -0,0 +1,13 @@
+package org.example.projekt2_gruppe5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt2Gruppe5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt2Gruppe5Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projekt2_gruppe5/Projekt2Gruppe5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projekt2_gruppe5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt2Gruppe5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
