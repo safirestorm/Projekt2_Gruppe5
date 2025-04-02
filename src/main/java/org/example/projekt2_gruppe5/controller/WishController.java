76d6cf2e20d95bb3f2b9786a0621d48983acc99a
hdtrap <peterpetertange@gmail.com>
2025-04-02 14:58:21 +0200

    Annotation of controllers and repositories

--- src/main/java/org/example/projekt2_gruppe5/controller/WishController.java
+++ src/main/java/org/example/projekt2_gruppe5/controller/WishController.java
@@ -1,4 +1,7 @@
 package org.example.projekt2_gruppe5.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class WishController {
 }
--- src/main/java/org/example/projekt2_gruppe5/repository/WishlistRepo.java
+++ src/main/java/org/example/projekt2_gruppe5/repository/WishlistRepo.java
@@ -1,4 +1,7 @@
 package org.example.projekt2_gruppe5.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class WishlistRepo {
 }
