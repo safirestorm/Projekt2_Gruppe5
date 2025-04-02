418f49891d1484d3daf4b0b7731f07b5cc52658e
safirestorm <sarahfagerstroem@gmail.com>
2025-04-02 15:49:30 +0200

    Tilføjet getter and setter og contructer til wish

--- src/main/java/org/example/projekt2_gruppe5/model/Wish.java
+++ src/main/java/org/example/projekt2_gruppe5/model/Wish.java
@@ -6,6 +6,54 @@ public class Wish {
     int price;
     String link;
     String image;
-    boolean isReserved;
+
+    public Wish(int wishId, String name, int price, String link, String image) {
+        this.wishId = wishId;
+        this.name = name;
+        this.price = price;
+        this.link = link;
+        this.image = image;
+    }
+
+    public int getWishId() {
+        return wishId;
+    }
+
+    public void setWishId(int wishId) {
+        this.wishId = wishId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
 
 }
