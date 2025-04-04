da2b2870b9814988ac2bfc29ec2b5e5487faf6e5
CoderFrede1010 <frfu0001@stud.kea.dk>
2025-04-04 11:44:26 +0200

    Opdatering i thymeleaf brugerside.html
    Og WishlistController

--- src/main/java/org/example/projekt2_gruppe5/controller/WishlistController.java
+++ src/main/java/org/example/projekt2_gruppe5/controller/WishlistController.java
@@ -1,10 +1,21 @@
 package org.example.projekt2_gruppe5.controller;
 
+import org.example.projekt2_gruppe5.repository.WishlistRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WishlistController {
 
+    @Autowired
+    private WishlistRepo wishlistRepository;
 
-
+    @GetMapping("/wishlists")
+    public String showWishlists(Model model) {
+        // får WishLists
+        model.addAttribute("wishListList", wishlistRepository.getAllWishlist());
+        return "brugerside"; // Thymeleaf will look for this .html template
+    }
 }
